d08dcfb9f4835213acb113fac218f5bcfb5f0fa1
diegotosato <tosato1998@gmail.com>
2025-10-15 14:39:42 +0200

    declare three variables for price ticket
    1st var entire price ticket
    2nd var calc discount price ticket for age under 18
    3rd var calc discount price ticket for age over 65

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,13 @@ const kilo = 10
 const age = prompt('age')
 
 
+//variabile per calcolo prezzo del biglietto intero
+let ticketPrice = kilo * 0.21
+
+//variabile per calcolo sconto minorenni (<18)
+const discountUnder = priceKilo * 0.8
+
+//variabile per calcolo sconto over 65 (>65)
+const discountOver = priceKilo * 0.6
+
 
