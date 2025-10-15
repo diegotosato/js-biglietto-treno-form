c687f09f47f9a89640df4f750a1afa415426f088
diegotosato <tosato1998@gmail.com>
2025-10-15 14:51:17 +0200

    declare two variables to isolate kilo and age input, get them by id

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,38 @@
-console.log('lavora');
+// //variabile per i chilometri
+// const kilo = 10
+
+// //variabile per l'età dell'utente
+// const age = 27
+
+
+// //variabile per calcolo prezzo del biglietto intero
+// let ticketPrice = kilo * 0.21
+
+// //variabile per calcolo sconto minorenni (<18)
+// const discountUnder = ticketPrice * 0.8
+
+// //variabile per calcolo sconto over 65 (>65)
+// const discountOver = ticketPrice * 0.6
 
 
 
-//variabile per i chilometri
-const kilo = 10
+// if (age < 18) {
+//     ticketPrice = discountUnder
+// } else if (age > 65) {
+//     ticketPrice = discountOver
+// }
 
-//variabile per l'età dell'utente
-const age = prompt('age')
-
-
-//variabile per calcolo prezzo del biglietto intero
-let ticketPrice = kilo * 0.21
-
-//variabile per calcolo sconto minorenni (<18)
-const discountUnder = priceKilo * 0.8
-
-//variabile per calcolo sconto over 65 (>65)
-const discountOver = priceKilo * 0.6
+// console.log(ticketPrice);
 
 
 
-if (age < 18) {
-    ticketPrice = discountUnder
-} else if (age > 65) {
-    ticketPrice = discountOver
-}
+/*---------------------------------------------------------------*/
 
-console.log(priceKilo);
+//costante per isolare l'input dei chilometri
+const kiloField = document.getElementById('kilo-field')
+
+//costante per isolare l'input dell'età del passeggero
+const ageField = document.getElementById('age-field')
+
+console.log(kiloField.value, ageField.value);
+
